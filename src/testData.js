e84de3658b9b2a20d830fa178f240cc3da8c0264
Joseph Frazier <1212jtraceur@gmail.com>
2017-11-18 11:34:35 -0500

    testData: Remove location names
    Google gets confused by them and it screws up the map links.

--- src/testData.js
+++ src/testData.js
@@ -4,25 +4,25 @@ const destination = `Saint Mary's Church, 440 Grand Street, New York, NY, United
 // https://farm3.static.flickr.com/2778/4134507221_d0c9ec1b7c_o.jpg
 const grid = [
   [
-    `Gristede's Foods, 221 8th Avenue, New York, NY, United States`,
-    `Gristedes, 907 8th Avenue, New York, NY, United States`,
-    `Pioneer Supermarket, 289 Columbus Avenue, New York, NY, United States`,
-    `Gristede's Foods, 25 University Place, New York, NY, United States`
+    `221 8th Avenue, New York, NY, United States`,
+    `907 8th Avenue, New York, NY, United States`,
+    `289 Columbus Avenue, New York, NY, United States`,
+    `25 University Place, New York, NY, United States`
   ],
   [
     `512 2nd Avenue, New York, NY, United States`,
-    `Food Emporium, 452 W 43rd St, New York, NY, United States`,
-    `Pioneer Supermarket, 1407 Lexington Avenue, New York, NY, United States`,
+    `452 W 43rd St, New York, NY, United States`,
+    `1407 Lexington Avenue, New York, NY, United States`,
     `316 Greenwich Street, New York, NY, United States`
   ],
   [
-    `Morton Williams Supermarkets, 311 E 23rd St, New York, NY, United States`,
+    `311 E 23rd St, New York, NY, United States`,
     `580 9th Avenue, New York, NY, United States`,
     `2704 Broadway, New York, NY, United States`,
-    `C-Town Supermarkets, 5 St James Pl, New York, NY, United States`
+    `5 St James Pl, New York, NY, United States`
   ],
   [
-    `The Food Emporium, 10 Union Square East, New York, NY, United States`,
+    `10 Union Square East, New York, NY, United States`,
     `225 West 57th Street, New York, NY, United States`,
     `609 Columbus Avenue, New York, NY, United States`,
     `2217 7th Avenue, New York, NY, United States`
@@ -30,7 +30,7 @@ const grid = [
 ]
 
 const babyFoodStops = [
-  `Hudson Guild, 441 West 26th Street, New York, NY, United States`,
+  `441 West 26th Street, New York, NY, United States`,
   `137 East 2nd Street, New York, NY, United States`
 ]
 

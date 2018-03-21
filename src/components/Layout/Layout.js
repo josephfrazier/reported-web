972ccdd0b5a31540e812248d98852e812a365a8d
Joseph Frazier <1212jtraceur@gmail.com>
2018-03-21 19:59:50 -0400

    Don't render Header/Feedback/Footer

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -14,9 +14,9 @@ import withStyles from 'isomorphic-style-loader/lib/withStyles';
 // external-global styles must be imported in your JS.
 import normalizeCss from 'normalize.css';
 import s from './Layout.css';
-import Header from '../Header';
-import Feedback from '../Feedback';
-import Footer from '../Footer';
+// import Header from '../Header';
+// import Feedback from '../Feedback';
+// import Footer from '../Footer';
 
 class Layout extends React.Component {
   static propTypes = {
@@ -26,10 +26,10 @@ class Layout extends React.Component {
   render() {
     return (
       <div>
-        <Header />
+        {/* <Header /> */}
         {this.props.children}
-        <Feedback />
-        <Footer />
+        {/* <Feedback /> */}
+        {/* <Footer /> */}
       </div>
     );
   }

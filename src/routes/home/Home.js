1035771b32261c466a81d6128bb6e517e11ff736
Joseph Frazier <1212jtraceur@gmail.com>
2018-04-17 15:04:26 -0400

    Add loading indicator for plate recognition
    Easier viewed with:
    
        git show --color-words

--- src/routes/home/Home.js
+++ src/routes/home/Home.js
@@ -121,6 +121,7 @@ class Home extends React.Component {
 
     isSubmitting: false,
     isLoadingImages: false,
+    isLoadingPlate: false,
   };
 
   componentDidMount() {
@@ -190,7 +191,10 @@ class Home extends React.Component {
       }),
     );
 
-    this.setState({ imageBytess: images.map(({ imageBytes }) => imageBytes) });
+    this.setState({
+      imageBytess: images.map(({ imageBytes }) => imageBytes),
+      isLoadingImages: false,
+    });
 
     for (const { imageBytes } of images) {
       try {
@@ -203,8 +207,6 @@ class Home extends React.Component {
         console.error(`Error: ${err.message}`);
       }
     }
-
-    this.setState({ isLoadingImages: false });
   };
 
   extractLocationDate = ({ imageBytes }) => {
@@ -230,30 +232,38 @@ class Home extends React.Component {
 
   // adapted from https://github.com/openalpr/cloudapi/tree/8141c1ba57f03df4f53430c6e5e389b39714d0e0/javascript#getting-started
   extractPlate = async ({ imageBytes }) => {
-    const country = 'us'; // {String} Defines the training data used by OpenALPR. \"us\" analyzes North-American style plates. \"eu\" analyzes European-style plates. This field is required if using the \"plate\" task You may use multiple datasets by using commas between the country codes. For example, 'au,auwide' would analyze using both the Australian plate styles. A full list of supported country codes can be found here https://github.com/openalpr/openalpr/tree/master/runtime_data/config
+    this.setState({ isLoadingPlate: true });
 
-    const opts = {
-      recognizeVehicle: 0, // {Integer} If set to 1, the vehicle will also be recognized in the image This requires an additional credit per request
-      state: 'ny', // {String} Corresponds to a US state or EU country code used by OpenALPR pattern recognition. For example, using \"md\" matches US plates against the Maryland plate patterns. Using \"fr\" matches European plates against the French plate patterns.
-      returnImage: 0, // {Integer} If set to 1, the image you uploaded will be encoded in base64 and sent back along with the response
-      topn: 10, // {Integer} The number of results you would like to be returned for plate candidates and vehicle classifications
-      prewarp: '', // {String} Prewarp configuration is used to calibrate the analyses for the angle of a particular camera. More information is available here http://doc.openalpr.com/accuracy_improvements.html#calibration
-    };
+    try {
+      const country = 'us'; // {String} Defines the training data used by OpenALPR. \"us\" analyzes North-American style plates. \"eu\" analyzes European-style plates. This field is required if using the \"plate\" task You may use multiple datasets by using commas between the country codes. For example, 'au,auwide' would analyze using both the Australian plate styles. A full list of supported country codes can be found here https://github.com/openalpr/openalpr/tree/master/runtime_data/config
 
-    const { data } = await axios.post('/openalpr', {
-      imageBytes,
-      country,
-      opts,
-    });
-    console.info(
-      `API called successfully. Returned data: ${JSON.stringify(
-        data,
-        null,
-        2,
-      )}`,
-    );
-    const { plate } = data.results[0];
-    this.setLicensePlate({ plate });
+      const opts = {
+        recognizeVehicle: 0, // {Integer} If set to 1, the vehicle will also be recognized in the image This requires an additional credit per request
+        state: 'ny', // {String} Corresponds to a US state or EU country code used by OpenALPR pattern recognition. For example, using \"md\" matches US plates against the Maryland plate patterns. Using \"fr\" matches European plates against the French plate patterns.
+        returnImage: 0, // {Integer} If set to 1, the image you uploaded will be encoded in base64 and sent back along with the response
+        topn: 10, // {Integer} The number of results you would like to be returned for plate candidates and vehicle classifications
+        prewarp: '', // {String} Prewarp configuration is used to calibrate the analyses for the angle of a particular camera. More information is available here http://doc.openalpr.com/accuracy_improvements.html#calibration
+      };
+
+      const { data } = await axios.post('/openalpr', {
+        imageBytes,
+        country,
+        opts,
+      });
+      console.info(
+        `API called successfully. Returned data: ${JSON.stringify(
+          data,
+          null,
+          2,
+        )}`,
+      );
+      const { plate } = data.results[0];
+      this.setLicensePlate({ plate });
+    } catch (err) {
+      throw err;
+    } finally {
+      this.setState({ isLoadingPlate: false });
+    }
   };
 
   extractDate = ({ exifData }) => {
@@ -451,6 +461,14 @@ class Home extends React.Component {
                   >
                     📍 &amp; 📅⌚
                   </button>
+                  &nbsp;
+                  <button
+                    onClick={() => {
+                      this.extractPlate({ imageBytes });
+                    }}
+                  >
+                    Read plate
+                  </button>
                 </li>
               );
             })}
@@ -472,7 +490,7 @@ class Home extends React.Component {
           </label>
 
           <label>
-            License/Medallion:{' '}
+            License/Medallion: {this.state.isLoadingPlate && 'Loading...'}
             <input
               type="text"
               value={this.state.plate}

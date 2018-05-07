6d8062d17f67982c6cbc3f6bf70789de5c2dc169
Joseph Frazier <1212jtraceur@gmail.com>
2018-05-07 15:10:07 -0400

    Add address bar theme color
    https://developers.google.com/web/tools/lighthouse/audits/address-bar
    
    The color was chosen to match the Android app:
    https://github.com/jeffrono/Reported-Android/search?utf8=%E2%9C%93&q=fbc02d&type=

--- src/components/Html.js
+++ src/components/Html.js
@@ -44,6 +44,7 @@ class Html extends React.Component {
           <title>{title}</title>
           <meta name="description" content={description} />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <meta name="theme-color" content="#fbc02d" />
           {scripts.map(script => (
             <link key={script} rel="preload" href={script} as="script" />
           ))}

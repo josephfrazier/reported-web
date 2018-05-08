3f67dfe3eabdf45bbd3e49c172d8dca7017fe5b8
Joseph Frazier <1212jtraceur@gmail.com>
2018-05-08 14:12:22 -0400

    Make Service Worker show "You need to be online to use Reported." when offline
    Android Chrome should now prompt the user to add the app to their
    homescreen. See here for details.
    https://developers.google.com/web/fundamentals/app-install-banners/
    
    The code for this was adapted from the following:
    
    * https://github.com/pwa-builder/serviceworkers/blob/5b9128ec9232556171f0969bc2aa9c6039054ed6/serviceWorker1/pwabuilder-sw.js
    * https://github.com/paulirish/caltrainschedule.io/pull/51
      * fixes https://github.com/paulirish/caltrainschedule.io/issues/49
      * chrome bug: https://bugs.chromium.org/p/chromium/issues/detail?id=823392

--- public/sw.js
+++ public/sw.js
@@ -1,1 +1,27 @@
-self.addEventListener('fetch', () => {}); // eslint-disable-line no-restricted-globals
+// Derived from https://github.com/pwa-builder/serviceworkers/blob/5b9128ec9232556171f0969bc2aa9c6039054ed6/serviceWorker1/pwabuilder-sw.js
+
+const offlineBody = 'You need to be online to use Reported.';
+
+// If any fetch fails, it will show `offlineBody`
+// Maybe this should be limited to HTML documents?
+// eslint-disable-next-line no-restricted-globals
+self.addEventListener('fetch', event => {
+  const { request } = event;
+
+  // DevTools opening will trigger these o-i-c requests, which this SW can't handle.
+  // There's probably more going on here, but I'd rather just ignore this problem. :)
+  // https://github.com/paulirish/caltrainschedule.io/issues/49
+  if (request.cache === 'only-if-cached' && request.mode !== 'same-origin') {
+    return;
+  }
+
+  event.respondWith(
+    fetch(request).catch(error => {
+      console.error({
+        message: `[PWA Builder] Network request Failed. Serving offline page ${error}`,
+        event,
+      });
+      return new Response(offlineBody);
+    }),
+  );
+});

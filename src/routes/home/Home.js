85a90476a86930c12af4d6a137561d960fca9ea3
Joseph Frazier <1212jtraceur@gmail.com>
2018-05-11 11:26:42 -0400

    Scale map to 3/4 viewport height, not hardcoded to 400px

--- src/routes/home/Home.js
+++ src/routes/home/Home.js
@@ -865,7 +865,7 @@ const MyMapComponent = compose(
   withProps({
     googleMapURL: `https://maps.googleapis.com/maps/api/js?key=${GOOGLE_MAPS_API_KEY}&v=3.exp&libraries=geometry,drawing,places`,
     loadingElement: <div style={{ height: `100%` }} />,
-    containerElement: <div style={{ height: `400px` }} />,
+    containerElement: <div style={{ height: `75vh` }} />,
     mapElement: <div style={{ height: `100%` }} />,
   }),
   withScriptjs,

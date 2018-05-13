2875974a8f0894c6d6b929af4821db894b74ce2a
Joseph Frazier <1212jtraceur@gmail.com>
2018-05-13 01:14:00 -0400

    Remove large copy/pasted OpenALPR comments

--- src/routes/home/Home.js
+++ src/routes/home/Home.js
@@ -374,14 +374,15 @@ class Home extends React.Component {
       const attachmentBytes = attachmentBuffer.toString('base64');
       console.timeEnd(`toString('base64') ${attachmentFile.name}`); // eslint-disable-line no-console
 
-      const country = 'us'; // {String} Defines the training data used by OpenALPR. \"us\" analyzes North-American style plates. \"eu\" analyzes European-style plates. This field is required if using the \"plate\" task You may use multiple datasets by using commas between the country codes. For example, 'au,auwide' would analyze using both the Australian plate styles. A full list of supported country codes can be found here https://github.com/openalpr/openalpr/tree/master/runtime_data/config
-
+      // See here for more info about OpenALPR options
+      // https://github.com/openalpr/cloudapi/tree/8141c1ba57f03df4f53430c6e5e389b39714d0e0/javascript#getting-started
+      const country = 'us';
       const opts = {
-        recognizeVehicle: 0, // {Integer} If set to 1, the vehicle will also be recognized in the image This requires an additional credit per request
-        state: 'ny', // {String} Corresponds to a US state or EU country code used by OpenALPR pattern recognition. For example, using \"md\" matches US plates against the Maryland plate patterns. Using \"fr\" matches European plates against the French plate patterns.
-        returnImage: 0, // {Integer} If set to 1, the image you uploaded will be encoded in base64 and sent back along with the response
-        topn: 10, // {Integer} The number of results you would like to be returned for plate candidates and vehicle classifications
-        prewarp: '', // {String} Prewarp configuration is used to calibrate the analyses for the angle of a particular camera. More information is available here http://doc.openalpr.com/accuracy_improvements.html#calibration
+        recognizeVehicle: 0,
+        state: 'ny',
+        returnImage: 0,
+        topn: 10,
+        prewarp: '',
       };
 
       const { data } = await axios.post('/openalpr', {

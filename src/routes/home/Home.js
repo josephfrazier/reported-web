8fc8aa5850720cd4233d179d7d7a6794e310fdb0
Joseph Frazier <1212jtraceur@gmail.com>
2018-05-22 16:18:42 -0400

    Move map search box to top-left, instead of Map/Satellite toggle

--- src/routes/home/Home.js
+++ src/routes/home/Home.js
@@ -1106,7 +1106,7 @@ const MyMapComponent = compose(
       center={position}
       ref={onRef}
       onCenterChanged={onCenterChanged}
-      options={{ gestureHandling: 'greedy' }}
+      options={{ mapTypeControl: false, gestureHandling: 'greedy' }}
     >
       <Marker position={position} />
       <SearchBox
@@ -1122,7 +1122,7 @@ const MyMapComponent = compose(
             border: `1px solid transparent`,
             width: `240px`,
             height: `32px`,
-            marginTop: `27px`,
+            marginTop: `6px`,
             padding: `0 12px`,
             borderRadius: `3px`,
             boxShadow: `0 2px 6px rgba(0, 0, 0, 0.3)`,

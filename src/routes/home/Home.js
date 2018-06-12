0897f2b6d0f8b9cfe713e77c2ed5f374418d9d01
Joseph Frazier <1212jtraceur@gmail.com>
2018-06-12 08:56:22 -0400

    Handle error in Service Request lookup (srlookup)

--- src/routes/home/Home.js
+++ src/routes/home/Home.js
@@ -1058,7 +1058,12 @@ class SubmissionDetails extends React.Component {
     const LoadableServiceRequestStatus = Loadable({
       loader: () =>
         axios.get(`/srlookup/${reqnumber}`).then(({ data }) => () => {
-          const { threeOneOneSRLookupResponse } = data;
+          const { error, threeOneOneSRLookupResponse } = data;
+          if (error) {
+            const { errorMessage, errorCode } = error;
+            return `${errorMessage} (error code ${errorCode})`;
+          }
+
           const items = Object.entries(threeOneOneSRLookupResponse[0]).map(
             ([key, value]) => (
               <React.Fragment key={key}>

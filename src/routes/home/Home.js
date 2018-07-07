c4012e969f2f712509e0e470cd377c058105ffe5
Joseph Frazier <1212jtraceur@gmail.com>
2018-07-07 10:52:47 -0400

    Add button to close map modal (#39)
    Fixes https://github.com/josephfrazier/Reported-Web/issues/36

--- src/routes/home/Home.js
+++ src/routes/home/Home.js
@@ -1058,6 +1058,15 @@ class Home extends React.Component {
                       });
                     }}
                   />
+
+                  <button
+                    onClick={() => this.setState({ isMapOpen: false })}
+                    style={{
+                      float: 'right',
+                    }}
+                  >
+                    Close
+                  </button>
                 </Modal>
 
                 <label>
@@ -1210,7 +1219,9 @@ const MyMapComponent = compose(
   withProps({
     googleMapURL: `https://maps.googleapis.com/maps/api/js?key=${GOOGLE_MAPS_API_KEY}&v=3.exp&libraries=geometry,drawing,places`,
     loadingElement: <div style={{ height: `100%` }} />,
-    containerElement: <div style={{ height: `100%` }} />,
+    containerElement: (
+      <div style={{ height: `calc(100% - (18px + 1.5rem))` }} />
+    ),
     mapElement: <div style={{ height: `100%` }} />,
   }),
   withScriptjs,

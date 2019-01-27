31dbfa853b20ac407da7bbf4dc9a1b9f4be88c6e
Joseph Frazier <1212jtraceur@gmail.com>
2019-01-27 12:27:31 -0500

    Add `--no-sandbox` to puppeteer.launch args
    See https://github.com/jontewks/puppeteer-heroku-buildpack#puppeteer-heroku-buildpack

--- src/311.js
+++ src/311.js
@@ -32,7 +32,10 @@ async function submit_311_illegal_parking_report({
 }) {
   const submission_date = new Date(submission_timestamp);
 
-  const browser = await puppeteer.launch({ headless: true });
+  const browser = await puppeteer.launch({
+    headless: true,
+    args: ['--no-sandbox'],
+  });
   const page = await browser.newPage();
 
   // Don't bother loading images, styles, or fonts. https://github.com/GoogleChrome/puppeteer/issues/1913#issuecomment-361224733

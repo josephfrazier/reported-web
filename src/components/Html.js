6523d249001cdc9a5245bd102561f6518baeb0ca
Joseph Frazier <1212jtraceur@gmail.com>
2020-02-25 19:19:03 -0500

    Update web app theme color (#220)
    Following up on commit 67bc3eb (https://github.com/josephfrazier/Reported-Web/pull/219),
    this updates the PWA color to be the "bike lane green" in the icon.

--- src/components/Html.js
+++ src/components/Html.js
@@ -44,7 +44,7 @@ class Html extends React.Component {
           <title>{title}</title>
           <meta name="description" content={description} />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
-          <meta name="theme-color" content="#fbc02d" />
+          <meta name="theme-color" content="#008000" />
           {scripts.map(script => (
             <link key={script} rel="preload" href={script} as="script" />
           ))}

b298fc90e52ab5e0b20893bd8ae3db096e1830ee
Joseph Frazier <1212jtraceur@gmail.com>
2021-02-11 03:14:01 -0500

    webpack config: css-loader: Fix localIdentName syntax: https://github.com/webpack-contrib/css-loader/blob/e76ef2c0667b143bfa383efe379fa67142664319/CHANGELOG.md#breaking-changes

--- tools/webpack.config.js
+++ tools/webpack.config.js
@@ -152,10 +152,11 @@ const config = {
               importLoaders: 1,
               sourceMap: isDebug,
               // CSS Modules https://github.com/css-modules/css-modules
-              modules: true,
-              localIdentName: isDebug
-                ? '[name]-[local]-[hash:base64:5]'
-                : '[hash:base64:5]',
+              modules: {
+                localIdentName: isDebug
+                  ? '[name]-[local]-[hash:base64:5]'
+                  : '[hash:base64:5]',
+              },
               // CSS Nano http://cssnano.co/
               // minimize: isDebug ? false : minimizeCssOptions,
             },

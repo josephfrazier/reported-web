798cffcf15c7702ee70d5635ba21adc3ec22e7b8
Joseph Frazier <1212jtraceur@gmail.com>
2021-03-04 22:12:26 -0500

    Indicate which metadata fields couldn't be extracted
    Fixes https://github.com/josephfrazier/Reported-Web/issues/251:
    
    > From my conversation with Rich at [reportedcab.slack.com/archives/C9VNM3DL4/p1612717445004500?thread_ts=1610546281.002400&cid=C9VNM3DL4](https://reportedcab.slack.com/archives/C9VNM3DL4/p1612717445004500?thread_ts=1610546281.002400&cid=C9VNM3DL4):
    >
    > > Ohhhh, you've stumbled across a quirk in how the error-handling is implemented, if I remember correctly: When I was first developing this, only one image was supported, so if it didn't find either the plate, date, or the location data, it would show the error message. Then, when I added support for multiple images, I didn't update the logic to take into account how many images there are.
    > > So, I'm guessing what happened here is that it couldn't find the plate in the second image, and showed the error message instead of waiting for the plate to be found in the first image.
    > > I'll update the error text to indicate that if multiple images are submitted, another one may have the required data, but there's a couple of bigger improvements that could be made:
    > >
    > > 1. Have the error message indicate which fields couldn't be found, instead of just saying "plate/location/date".
    > > 2. If there are multiple images, check all of them before showing the error message.
    
    This addresses the first item.

--- src/routes/home/Home.js
+++ src/routes/home/Home.js
@@ -124,31 +124,43 @@ function extractLocationDateFromVideo({ attachmentArrayBuffer }) {
 }
 
 async function extractLocation({ attachmentFile, attachmentArrayBuffer, ext }) {
-  if (isVideo({ ext })) {
-    return extractLocationDateFromVideo({ attachmentArrayBuffer })[0];
-  }
-  if (!isImage({ ext })) {
-    throw new Error(`${attachmentFile.name} is not an image/video`);
-  }
+  try {
+    if (isVideo({ ext })) {
+      return extractLocationDateFromVideo({ attachmentArrayBuffer })[0];
+    }
+    if (!isImage({ ext })) {
+      throw new Error(`${attachmentFile.name} is not an image/video`);
+    }
 
-  const { latitude, longitude } = await exifr.gps(attachmentArrayBuffer);
+    const { latitude, longitude } = await exifr.gps(attachmentArrayBuffer);
 
-  return { latitude, longitude };
+    return { latitude, longitude };
+  } catch (err) {
+    console.error(err);
+
+    throw 'location'; // eslint-disable-line no-throw-literal
+  }
 }
 
 async function extractDate({ attachmentFile, attachmentArrayBuffer, ext }) {
-  if (isVideo({ ext })) {
-    return extractLocationDateFromVideo({ attachmentArrayBuffer })[1];
-  }
-  if (!isImage({ ext })) {
-    throw new Error(`${attachmentFile.name} is not an image/video`);
-  }
+  try {
+    if (isVideo({ ext })) {
+      return extractLocationDateFromVideo({ attachmentArrayBuffer })[1];
+    }
+    if (!isImage({ ext })) {
+      throw new Error(`${attachmentFile.name} is not an image/video`);
+    }
 
-  const { CreateDate } = await exifr.parse(attachmentArrayBuffer, [
-    'CreateDate',
-  ]);
+    const { CreateDate } = await exifr.parse(attachmentArrayBuffer, [
+      'CreateDate',
+    ]);
 
-  return CreateDate.getTime();
+    return CreateDate.getTime();
+  } catch (err) {
+    console.error(err);
+
+    throw 'creation date'; // eslint-disable-line no-throw-literal
+  }
 }
 
 // derived from https://github.com/feross/capture-frame/tree/06b8f5eac78fea305f7f577d1697ee3b6999c9a8#complete-example
@@ -483,7 +495,7 @@ class Home extends React.Component {
         const { ext } = await FileType.fromBuffer(attachmentBuffer);
 
         // eslint-disable-next-line no-await-in-loop
-        await Promise.all([
+        await Promise.allSettled([
           this.extractPlate({ attachmentFile, attachmentBuffer, ext }),
           extractDate({
             attachmentFile,
@@ -495,8 +507,16 @@ class Home extends React.Component {
             attachmentArrayBuffer,
             ext,
           }).then(this.setCoords),
-        ]);
-      } catch (err) {
+        ]).then(values => {
+          const rejected = values.filter(v => v.status === 'rejected');
+
+          if (rejected.length === 0) {
+            return;
+          }
+
+          throw rejected.map(v => v.reason).join(', ');
+        });
+      } catch (missingValuesString) {
         const hasMultipleAttachments = attachmentData.length > 1;
         const fileCopy = hasMultipleAttachments
           ? 'one of the files, but they may have been found in other files.'
@@ -505,14 +525,11 @@ class Home extends React.Component {
         this.alert(
           <React.Fragment>
             <p>
-              Could not extract plate and/or location and/or date from{' '}
-              {fileCopy} Please enter/confirm any missing values manually.
+              Could not extract the {missingValuesString} from {fileCopy} Please
+              enter/confirm any missing values manually.
             </p>
-
-            <p style={{ whiteSpace: 'pre-wrap' }}>{err.stack}</p>
           </React.Fragment>,
         );
-        console.error(err.stack);
       }
     }
   };
@@ -575,7 +592,9 @@ class Home extends React.Component {
       this.attachmentPlates.set(attachmentFile, result);
       return result;
     } catch (err) {
-      throw err;
+      console.error(err);
+
+      throw 'license plate'; // eslint-disable-line no-throw-literal
     } finally {
       console.timeEnd('extractPlate'); // eslint-disable-line no-console
     }

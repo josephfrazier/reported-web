afdd68a17d8dd67ec3ec11690a00bae94a8b4322
Joseph Frazier <1212jtraceur@gmail.com>
2023-01-23 19:19:02 -0500

    Don't re-specify toast properties in `toast.warn` call

--- src/routes/home/Home.js
+++ src/routes/home/Home.js
@@ -670,17 +670,7 @@ class Home extends React.Component {
         console.error(err);
       });
 
-  alert = modalText =>
-    toast.warn(modalText, {
-      position: 'top-center',
-      autoClose: 5000,
-      hideProgressBar: false,
-      closeOnClick: true,
-      pauseOnHover: true,
-      draggable: true,
-      progress: undefined,
-      theme: 'dark',
-    });
+  alert = modalText => toast.warn(modalText);
 
   loadPreviousSubmissions = () => {
     axios

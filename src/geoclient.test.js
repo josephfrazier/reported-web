2a365a304bced7db56dfc75cd94183ed1961f7ca
Joseph Frazier <1212jtraceur@gmail.com>
2023-03-19 11:22:47 -0400

    XXX Run only the failing test to isolate it

--- src/geoclient.test.js
+++ src/geoclient.test.js
@@ -5,44 +5,44 @@
 
 import { validateLocation, processValidation } from './geoclient.js';
 
-describe('validateLocation', () => {
-  test('returns the right object', async () => {
-    const result = await validateLocation({
-      lat: 40.713007655065155,
-      long: -74.00592677275506,
-    });
+// describe('validateLocation', () => {
+//   test('returns the right object', async () => {
+//     const result = await validateLocation({
+//       lat: 40.713007655065155,
+//       long: -74.00592677275506,
+//     });
 
-    expect(result).toMatchSnapshot();
-  });
-});
+//     expect(result).toMatchSnapshot();
+//   });
+// });
 
 describe('processValidation', () => {
-  test('returns the right object', async () => {
-    const result = await processValidation({
-      lat: 40.7128,
-      long: -74.006,
-    });
+  // test('returns the right object', async () => {
+  //   const result = await processValidation({
+  //     lat: 40.7128,
+  //     long: -74.006,
+  //   });
 
-    expect(result).toMatchSnapshot();
-  });
+  //   expect(result).toMatchSnapshot();
+  // });
 
-  test('returns the right object for string lat/long', async () => {
-    const result = await processValidation({
-      lat: '40.772204329580426',
-      long: '-73.92590668144963',
-    });
+  // test('returns the right object for string lat/long', async () => {
+  //   const result = await processValidation({
+  //     lat: '40.772204329580426',
+  //     long: '-73.92590668144963',
+  //   });
 
-    expect(result).toMatchSnapshot();
-  });
+  //   expect(result).toMatchSnapshot();
+  // });
 
-  test('returns the right object around 3521-3501 Riverdale Ave, The Bronx, NY 10463', async () => {
-    const result = await processValidation({
-      lat: 40.88067222222222,
-      long: -73.91039722222223,
-    });
+  // test('returns the right object around 3521-3501 Riverdale Ave, The Bronx, NY 10463', async () => {
+  //   const result = await processValidation({
+  //     lat: 40.88067222222222,
+  //     long: -73.91039722222223,
+  //   });
 
-    expect(result).toMatchSnapshot();
-  });
+  //   expect(result).toMatchSnapshot();
+  // });
 
   test('returns the right object around 40.6435893,-73.7820064', async () => {
     const [lat, long] = [40.6435893, -73.7820064];

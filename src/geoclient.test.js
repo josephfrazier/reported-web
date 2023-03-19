39544d43e4a86eaca0a3146301d074d226d4091c
Joseph Frazier <1212jtraceur@gmail.com>
2023-03-19 11:14:01 -0400

    WIP Test processValidation around 40.6435893,-73.7820064
    See context at https://reportedcab.slack.com/archives/C9VNM3DL4/p1679189863760369?thread_ts=1676288095.715499&cid=C9VNM3DL4

--- src/geoclient.test.js
+++ src/geoclient.test.js
@@ -43,4 +43,11 @@ describe('processValidation', () => {
 
     expect(result).toMatchSnapshot();
   });
+
+  test('returns the right object around 40.6435893,-73.7820064', async () => {
+    const [lat, long] = [40.6435893, -73.7820064];
+    const result = await processValidation({ lat, long });
+
+    expect(result).toMatchSnapshot();
+  });
 });

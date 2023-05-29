85fa261cd8926b07a53956c862bee2a26bc2f209
Joseph Frazier <1212jtraceur@gmail.com>
2023-05-29 14:06:56 -0400

    Remove stale comments from adding DC to license plate states

--- src/routes/home/Home.js
+++ src/routes/home/Home.js
@@ -35,7 +35,6 @@ import omit from 'object.omit';
 import bufferToArrayBuffer from 'buffer-to-arraybuffer';
 import objectToFormData from 'object-to-formdata';
 import usStateNames from 'datasets-us-states-abbr-names';
-// import usStateNamesWithoutDC from 'datasets-us-states-abbr-names';
 import fileExtension from 'file-extension';
 import diceware from 'diceware-generator';
 import wordlist from 'diceware-wordlist-en-eff';
@@ -55,10 +54,6 @@ import { isImage, isVideo } from '../../isImage.js';
 import getNycTimezoneOffset from '../../timezone.js';
 import { getBoroNameMemoized } from '../../getBoroName.js';
 
-// const usStateNames = {
-//   ...usStateNames,
-//   DC: 'District of Columbia'
-// }
 usStateNames.DC = 'District of Columbia';
 
 const GOOGLE_MAPS_API_KEY = 'AIzaSyDlwm2ykA0ohTXeVepQYvkcmdjz2M2CKEI';

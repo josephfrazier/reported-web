d23cec0aef349c7349d90582baa3931319b8de36
Joseph Frazier <1212jtraceur@gmail.com>
2024-06-04 15:42:55 -0400

    srlookup: Extract UTC `Date Closed`, `Date Reported`, and `Updated On` dates from 311 response
    See https://reportedcab.slack.com/archives/C9VNM3DL4/p1717528491222359?thread_ts=1717450546.216129&cid=C9VNM3DL4
    
    > aha, it looks like there's an inline script that changes the dates from UTC to EST:
    >
    >     <!-- Formatting the dates to local timezone -->
    >     <script>
    >     $(()=>{
    >
    >         $("#srdatereported").text(getESTDate("2024-05-22T18:35:11Z"));
    >
    >
    >         $("#srupdatedon").text(getESTDate("2024-05-22T18:35:20Z"));
    >
    >
    >     });
    >     function getESTDate(utcdatestr) {
    >       return new Date(utcdatestr).toLocaleString('en-US', {
    >           year: 'numeric',
    >           month: '2-digit',
    >           day: '2-digit',
    >           hour: '2-digit',
    >           minute: '2-digit'
    >       })
    >     }
    >     </script>
    >
    > so I could either run that script, or scrape it's source code and extract the dates from it
    
    I'm going with UTC for now, but may consider changing it to EST later
    since that would help Jeff, see https://reportedcab.slack.com/archives/C9VNM3DL4/p1717529202165399?thread_ts=1717450546.216129&cid=C9VNM3DL4
    
    > Joe Frazier
    >   18 minutes ago
    > @jeff
    >  I think I'm getting this close to working, would you prefer UTC dates or EST dates?
    >
    >
    >
    >
    >
    > jeff
    >   < 1 minute ago
    > oh EST would be great
    >
    >
    > jeff
    >   < 1 minute ago
    > !
    >
    >
    > jeff
    >   < 1 minute ago
    > thx

--- src/srlookup.test.js
+++ src/srlookup.test.js
@@ -7,7 +7,7 @@ import srlookup from './srlookup.js';
 
 describe('srlookup', () => {
   test('returns the right object', async () => {
-    const result = await srlookup({ reqnumber: '311-18685922' });
+    const result = await srlookup({ reqnumber: '311-00039062' });
 
     expect(result).toMatchSnapshot();
   });

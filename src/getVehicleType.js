8903e3e9fdee84a114521c16ed59423cc3e1a39c
Joseph Frazier <1212jtraceur@gmail.com>
2024-06-04 13:14:48 -0400

    Update getVehicleType carfax.com request headers to pass test

--- src/getVehicleType.js
+++ src/getVehicleType.js
@@ -13,18 +13,25 @@ export default function getVehicleType({ licensePlate, licenseState }) {
   return axios
     .get(url, {
       headers: {
+        'User-Agent':
+          'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:126.0) Gecko/20100101 Firefox/126.0',
         Accept:
           'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
         'Accept-Language': 'en-US,en;q=0.5',
+        'Accept-Encoding': 'gzip, deflate, br, zstd',
         'Alt-Used': 'www.carfax.com',
-        'Cache-Control': 'no-cache',
-        Pragma: 'no-cache',
+        Connection: 'keep-alive',
+        Cookie:
+          'datadome=P4DhyQxRsHnufRFk6_p~IapRMG9Uk4_uAsTRiy0HzgtQRoMryslv1_EM6L0sp0m04MbtmbiEeMg_jeXV61cw4OdgmFcCM_8n5fp456S2ttWta~5i2psWkw_O3TIKP~IE',
+        'Upgrade-Insecure-Requests': '1',
         'Sec-Fetch-Dest': 'document',
         'Sec-Fetch-Mode': 'navigate',
-        'Sec-Fetch-Site': 'cross-site',
-        'Upgrade-Insecure-Requests': '1',
-        'User-Agent':
-          'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:121.0) Gecko/20100101 Firefox/121.0',
+        'Sec-Fetch-Site': 'none',
+        'Sec-Fetch-User': '?1',
+        Priority: 'u=1',
+        Pragma: 'no-cache',
+        'Cache-Control': 'no-cache',
+        TE: 'trailers',
       },
     })
     .then(({ data }) => {

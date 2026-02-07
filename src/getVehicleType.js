18cfd748146b2d9c3f33b3c343d703ac9900c332
Joseph Frazier <1212jtraceur@gmail.com>
2026-02-07 09:56:34 -0500

    Remove https import, use `finally` to end timer

--- src/getVehicleType.js
+++ src/getVehicleType.js
@@ -1,4 +1,3 @@
-import https from 'https';
 import axios from 'axios';
 
 // ported from https://github.com/jeffrono/Reported/blob/19b588171315a3093d53986f9fb995059f5084b4/v2/enrich_functions.rb#L325-L346
@@ -6,19 +5,23 @@ export default async function getVehicleType({ licensePlate, licenseState }) {
   const url = `https://api.lookupaplate.com/api/v1/wait_for_vehicle_details/${licenseState}/${licensePlate}/`;
 
   console.time(url); // eslint-disable-line no-console
-  const { data } = await axios.get(url);
-  console.timeEnd(url); // eslint-disable-line no-console
 
-  const vehicleJson = data.vehicle_json || {};
+  try {
+    const { data } = await axios.get(url);
 
-  return {
-    result: {
-      vehicleYear: vehicleJson['29'] || undefined,
-      vehicleMake: vehicleJson['26'] || undefined,
-      vehicleModel: vehicleJson['28'] || undefined,
-      vehicleBody: vehicleJson['5'] || undefined,
-      licensePlate,
-      licenseState,
-    },
-  };
+    const vehicleJson = data.vehicle_json || {};
+
+    return {
+      result: {
+        vehicleYear: vehicleJson['29'] || undefined,
+        vehicleMake: vehicleJson['26'] || undefined,
+        vehicleModel: vehicleJson['28'] || undefined,
+        vehicleBody: vehicleJson['5'] || undefined,
+        licensePlate,
+        licenseState,
+      },
+    };
+  } finally {
+    console.timeEnd(url); // eslint-disable-line no-console
+  }
 }

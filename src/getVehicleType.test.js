4bff9765aa695f5cbb449d7f8b138a94f26a34fc
Joseph Frazier <1212jtraceur@gmail.com>
2026-02-07 01:00:09 -0500

    Reapply "getVehicleType: Use puppeteer to drive a browser instead of just axios/jsdom" (#665)
    This reverts commit 075e6fb7d36af89a682d4e35d4c93823ad7a1c6b.

--- src/getVehicleType.test.js
+++ src/getVehicleType.test.js
@@ -3,15 +3,25 @@
  */
 /* eslint-env jest */
 
-import getVehicleType from './getVehicleType.js';
+import getVehicleType, { closeBrowser } from './getVehicleType.js';
+
+const timeoutMilliseconds = 10 * 1000;
 
 describe('getVehicleType', () => {
-  test.skip('returns the right object', async () => {
-    const result = await getVehicleType({
-      licensePlate: 'T716540C',
-      licenseState: 'NY',
-    });
-
-    expect(result).toMatchSnapshot();
+  afterAll(async () => {
+    await closeBrowser();
   });
+
+  test(
+    'returns the right object',
+    async () => {
+      const result = await getVehicleType({
+        licensePlate: 'TEST',
+        licenseState: 'NY',
+      });
+
+      expect(result).toMatchSnapshot();
+    },
+    timeoutMilliseconds,
+  );
 });

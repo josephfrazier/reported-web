8e9b87b0eacdca8a2ac78d1c38b3ae46d8db9998
Joseph Frazier <1212jtraceur@gmail.com>
2026-02-07 10:53:46 -0500

    Upgrade @babel/* packages to fix compatibility with Jest 30
    babel-jest 30 pulls in @babel/plugin-syntax-import-attributes, which
    requires @babel/core >=7.22.0. This upgrades @babel/core from 7.12.13
    to 7.29.0 (along with all other @babel/* packages), replaces the
    deprecated plugin-proposal-class-properties with
    plugin-transform-class-properties, removes plugin-syntax-dynamic-import
    (now included in preset-env), and deduplicates the yarn.lock to fix a
    nested @babel/types version mismatch.

--- babel.config.js
+++ babel.config.js
@@ -23,8 +23,7 @@ module.exports = {
     '@babel/preset-react',
   ],
   plugins: [
-    '@babel/plugin-proposal-class-properties',
-    '@babel/plugin-syntax-dynamic-import',
+    '@babel/plugin-transform-class-properties',
   ],
   ignore: ['node_modules', 'build'],
 };

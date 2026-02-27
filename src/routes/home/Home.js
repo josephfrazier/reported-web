6b683b7d6ad00718afa6396a870a80f2b727d97d
Joseph Frazier <1212jtraceur@gmail.com>
2026-02-27 17:31:42 -0500

    mvoe space outside brackets

--- src/routes/home/Home.js
+++ src/routes/home/Home.js
@@ -762,7 +762,7 @@ class Home extends React.Component {
               <React.Fragment>
                 {totalViolations} violation
                 {totalViolations !== 1 ? 's' : ''} found{' '}
-                {make && `(${color} ${make} ${body}) `}— ${fined.toFixed(2)}{' '}
+                {make && `(${color} ${make} ${body})`} — ${fined.toFixed(2)}{' '}
                 fined, ${outstanding.toFixed(2)} outstanding
                 {' ('}
                 <a href={detailsUrl} target="_blank" rel="noopener noreferrer">

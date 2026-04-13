41df4915bd57f31f20a01c04d32a72b740f415ed
Joseph Frazier <1212jtraceur@gmail.com>
2026-04-12 20:43:19 -0400

    Add "Maybe:" before howsmydriving vehicle color/make/body, since it's not particularly accurate currently

--- src/routes/home/Home.js
+++ src/routes/home/Home.js
@@ -752,7 +752,7 @@ class Home extends React.Component {
               <React.Fragment>
                 {totalViolations} violation
                 {totalViolations !== 1 ? 's' : ''} found{' '}
-                {make && `(${color} ${make} ${body})`} — ${fined.toFixed(2)}{' '}
+                {make && `(Maybe: ${color} ${make} ${body})`} — ${fined.toFixed(2)}{' '}
                 fined, ${outstanding.toFixed(2)} outstanding
                 {' ('}
                 <a href={detailsUrl} target="_blank" rel="noopener noreferrer">

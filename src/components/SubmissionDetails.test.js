1a026e6168a5c66857441b2d6a0ef61de4c532da
Joseph Frazier <1212jtraceur@gmail.com>
2026-06-19 15:26:42 -0400

    SubmissionDetails: Update 'delete button' test to have accurate submission.reqnumber: `N/A until submitted to 311`

--- src/components/SubmissionDetails.test.js
+++ src/components/SubmissionDetails.test.js
@@ -85,6 +85,7 @@ describe('SubmissionDetails', () => {
 
   test('renders delete button correctly', () => {
     const submission = {
+      reqnumber: 'N/A until submitted to 311',
       medallionNo: 'medallionNo',
       state: 'licenseState',
       typeofcomplaint: 'typeofcomplaint',

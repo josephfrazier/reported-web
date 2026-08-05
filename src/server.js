b7f7ebae10af8bcda09d22bf6f8c482aece47788
Joseph Frazier <1212jtraceur@gmail.com>
2026-08-05 16:32:28 -0400

    Revert "fix: replace file-type with mime-bytes"

--- src/server.js
+++ src/server.js
@@ -18,7 +18,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/server';
 import PrettyError from 'pretty-error';
 import Parse from 'parse/node';
-import { detectFromBuffer } from 'mime-bytes/file-type-detector';
+import FileType from 'file-type/browser';
 import multer from 'multer';
 import stringify from 'json-stringify-safe';
 import StyleContext from 'isomorphic-style-loader/StyleContext';
@@ -487,7 +487,7 @@ app.use('/submit', (req, res) => {
         const attachmentsWithFormats = await Promise.all(
           attachmentData.map(async ({ buffer: attachmentBuffer }) => ({
             attachmentBuffer,
-            ext: (await detectFromBuffer(attachmentBuffer))?.name || 'jpg',
+            ext: (await FileType.fromBuffer(attachmentBuffer)).ext,
           })),
         );
 

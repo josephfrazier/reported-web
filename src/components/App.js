bddb309d97f1159c48c97c364b0e3ddb838a45f5
Joseph Frazier <1212jtraceur@gmail.com>
2026-08-09 15:04:56 -0400

    fixup! Add intentional lint violations to test auto-fix workflow

--- src/components/App.js
+++ src/components/App.js
@@ -7,7 +7,7 @@
  * LICENSE.txt file in the root directory of this source tree.
  */
 
-import React from 'react';
+import React from "react";
 import PropTypes from 'prop-types';
 
 const ContextType = {
